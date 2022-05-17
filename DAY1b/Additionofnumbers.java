4cf9f48aab173a38e96d1e4d39c9e62511864a9f
unknown <mounikamc6302@gmail.com>
2022-05-17 14:35:08 +0000

    number

---
+++ DAY1b/Additionofnumbers.java
@@ -0,0 +1,14 @@
+package program1;
+
+public class Additionofnumbers {
+
+	public static void main(String[] args) {
+	    int number1 = 10;
+	    int number2 = 20;
+	    int result = number1 + number2;
+	    System.out.print("Sum of number" + result);
+	    
+
+	}
+
+}
