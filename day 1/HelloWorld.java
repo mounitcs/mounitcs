71fdecf30f9bc887b535a481b4de5604e1c35c5d
unknown <mounikamc6302@gmail.com>
2022-05-17 08:21:43 +0000

    hello

---
+++ day 1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author sfjbs
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+	}
+
+}
